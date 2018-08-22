0238c783f19d343935ee2907b0d04c7fc9a1cb2c
Kizito Akhilome <kizito@akhilo.me>
2018-08-22 22:22:22 +0100

    Finish module 3

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -1,9 +1,12 @@
-exports.myMiddleWare = (req, res, next) => {
-  req.name = 'Kay';
-  next();
-}
-
 exports.homePage = (req, res) => {
   console.log(req.name);
-  res.render('hello');
+  res.render('index');
+}
+
+exports.addStore = (req, res) => {
+  res.render('editStore', { title: 'Add Store' });
+}
+
+exports.createStore = (req, res) => {
+  res.json(req.body);
 }
