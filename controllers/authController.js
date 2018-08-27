9259d15e27178e285c1ef84a74cddd1d4408fb17
Kizito Akhilome <kizito@akhilo.me>
2018-08-27 15:36:40 +0100

    finish(m8v25): create login-logout sequence
    + protect /add route from non-authenticated users
    + get gravatar image from user email

--- controllers/authController.js
+++ controllers/authController.js
@@ -6,3 +6,19 @@ exports.login = passport.authenticate('local', {
   successRedirect: '/',
   successFlash: 'You are now logged in!' 
 });
+
+exports.logout = (req, res) => {
+  req.logout();
+  req.flash('success', 'You are now logged out!');
+  res.redirect('/');
+}
+
+exports.isLoggedIn = (req, res, next) => {
+  // first check if the user is authenticated
+  if (req.isAuthenticated()) {
+    return next(); // carry on! this one's legit
+  }
+
+  req.flash('error', 'You should be logged to see this page');
+  res.redirect('/login');
+}
