984e4e0b95d39cb0807516c395daa101961da32e
Kizito Akhilome <kizito@akhilo.me>
2018-08-27 12:32:47 +0100

    finish(m8v23): create and validate user accounts
    + create register and login views
    + create user model and add schema
    + create user controller and validate user input during registration
    + add register and login routes

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const storeController = require('../controllers/storeController');
+const userController = require('../controllers/userController');
 const { catchErrors } = require('../handlers/errorHandlers');
 
 // Do work here
@@ -26,6 +27,17 @@ router.get('/store/:slug', catchErrors(storeController.getStoreBySlug));
 
 router.get('/tags', catchErrors(storeController.getStoresByTag));
 router.get('/tags/:tag', catchErrors(storeController.getStoresByTag));
+
+router.get('/login', userController.loginForm);
+router.get('/register', userController.registerForm);
+
+// 1. Validate registration data
+
+// 2. Register the user
+
+// 3. Log them in
+
+router.post('/register', userController.validateRegister);
 
 
 
